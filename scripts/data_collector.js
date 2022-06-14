9ab86621c7874c5fa397b8aad1525da6ff9e3ec8
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2022-06-14 12:38:30 +0300

    add function "add user"

--- scripts/data_collector.js
+++ scripts/data_collector.js
@@ -1,39 +1,3 @@
-document.getElementById('edit_user_button').addEventListener('click', function () {
-    userEdit();
-});
-
-document.getElementById('add_scanner_button').addEventListener('click', function () {
-    addScanner();
-});
-
-document.getElementById('delete_scanner_button').addEventListener('click', function () {
-    deleteScanner();
-});
-
-document.getElementById('clear_history_button').addEventListener('click', function () {
-    clearActivityHistory();
-});
-
-document.getElementById('delete_user').addEventListener('click', function () {
-    deleteUser();
-});
-
-document.getElementById('search_button').addEventListener('click', function () {
-    search();
-});
-
-function setRole(role) {
-    if (role === "USER") {
-        document.getElementById('user').setAttribute('selected', true);
-    } else {
-        if (role === "ADMIN") {
-            document.getElementById('admin').setAttribute('selected', true);
-        } else {
-            document.getElementById('choose').setAttribute('selected', true);
-        }
-    }
-}
-
 function userEdit() {
     let id = document.getElementById('user_id').value;
     let surname = document.getElementById('surname').value;
@@ -62,6 +26,15 @@ function clearActivityHistory() {
 function deleteUser() {
     let id = document.getElementById('user_id').value;
     sendDeleteUser(id);
+}
+
+function addUser() {
+    let cardId = document.getElementById('card_id').value;
+    let surname = document.getElementById('surname').value;
+    let name = document.getElementById('name').value;
+    let patronymic = document.getElementById('patronymic').value;
+    let role = document.getElementById('role_select').value;
+    sendAddUser(cardId, surname, name, patronymic, role);
 }
 
 function search() {
