d12c3b806d5dcd975f88e83d2f21b4498b07755a
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2022-06-15 00:44:01 +0300

    add validation

--- scripts/data_collector.js
+++ scripts/data_collector.js
@@ -1,43 +1,51 @@
 function userEdit() {
-    let id = document.getElementById('user_id').value;
-    let surname = document.getElementById('surname').value;
-    let name = document.getElementById('name').value;
-    let patronymic = document.getElementById('patronymic').value;
-    let role = document.getElementById('role_select').value;
-    sendUserEdit(id, surname, name, patronymic, role);
+    let id = document.getElementById('user_id').value
+    let surname = document.getElementById('surname').value
+    let name = document.getElementById('name').value
+    let patronymic = document.getElementById('patronymic').value
+    let role = convertRole(document.getElementById('role_select').value)
+    if (userEditValidation(surname, name, role)) {
+        sendUserEdit(id, surname, name, patronymic, role)
+    }
 }
 
 function addScanner() {
-    let id = document.getElementById('add_scanner_input').value;
-    let role = document.getElementById('scanner_role_select').value;
-    sendAddScanner(id, role);
+    let id = document.getElementById('add_scanner_input').value
+    let role = convertRole(document.getElementById('scanner_role_select').value)
+    if (addScannerValidation(id, role)) {
+        sendAddScanner(id, role)
+    }
 }
 
 function deleteScanner() {
-    let id = document.getElementById('delete_scanner_input').value;
-    sendDeleteScanner(id);
+    let id = document.getElementById('delete_scanner_input').value
+    if (deleteScannerValidation(id)) {
+        sendDeleteScanner(id)
+    }
 }
 
 function clearActivityHistory() {
-    let id = document.getElementById('user_id').value;
-    sendClearActivityHistory(id);
+    let id = document.getElementById('user_id').value
+    sendClearActivityHistory(id)
 }
 
 function deleteUser() {
-    let id = document.getElementById('user_id').value;
-    sendDeleteUser(id);
+    let id = document.getElementById('user_id').value
+    sendDeleteUser(id)
 }
 
 function addUser() {
-    let cardId = document.getElementById('card_id').value;
-    let surname = document.getElementById('surname').value;
-    let name = document.getElementById('name').value;
-    let patronymic = document.getElementById('patronymic').value;
-    let role = document.getElementById('role_select').value;
-    sendAddUser(cardId, surname, name, patronymic, role);
+    let cardId = document.getElementById('card_id').value
+    let surname = document.getElementById('surname').value
+    let name = document.getElementById('name').value
+    let patronymic = document.getElementById('patronymic').value
+    let role = convertRole(document.getElementById('role_select').value)
+    if (addUserValidation(cardId, surname, name, role)) {
+        sendAddUser(cardId, surname, name, patronymic, role)
+    }
 }
 
 function search() {
-    let text = document.getElementById('search_input').value;
-    sendSearch(text);
+    let cardId = document.getElementById('search_input').value
+    sendSearch(cardId)
 }
---
+++ scripts/validator.js
@@ -0,0 +1,59 @@
+function addScannerValidation(id, role) {
+    let idValid = lengthValidation(id)
+    let roleValid = roleValidation(role)
+    addRedShadowIfInvalid(idValid, 'add_scanner_input')
+    addRedShadowIfInvalid(roleValid, 'scanner_role_select')
+    return idValid && roleValid
+}
+
+function addUserValidation(cardId, surname, name, role) {
+    let cardIdValid = lengthValidation(cardId)
+    let surnameValid = lengthValidation(surname)
+    let nameValid = lengthValidation(name)
+    let roleValid = roleValidation(role)
+    addRedShadowIfInvalid(cardIdValid, 'card_id')
+    addRedShadowIfInvalid(surnameValid, 'surname')
+    addRedShadowIfInvalid(nameValid, 'name')
+    addRedShadowIfInvalid(roleValid, 'role_select')
+    return cardIdValid && surnameValid && nameValid && roleValid
+}
+
+function userEditValidation(surname, name, role) {
+    let surnameValid = lengthValidation(surname)
+    let nameValid = lengthValidation(name)
+    let roleValid = roleValidation(role)
+    addRedShadowIfInvalid(surnameValid, 'surname')
+    addRedShadowIfInvalid(nameValid, 'name')
+    addRedShadowIfInvalid(roleValid, 'role_select')
+    return surnameValid && nameValid && roleValid
+}
+
+function deleteScannerValidation(id) {
+    let idValid = lengthValidation(id)
+    addRedShadowIfInvalid(idValid, 'delete_scanner_input')
+    return idValid
+}
+
+function addRedShadowIfInvalid(valid, elementId) {
+    if (valid) {
+        document.getElementById(elementId).style.boxShadow = ''
+    } else {
+        document.getElementById(elementId).style.boxShadow = '0 0 5px 1px red'
+    }
+}
+
+function lengthValidation(id) {
+    if (id.length > 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function roleValidation(role) {
+    if (role === 'ADMIN' || role === 'USER') {
+        return true
+    } else {
+        return false
+    }
+}
